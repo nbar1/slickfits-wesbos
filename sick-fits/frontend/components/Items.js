c06e84a78ef3351954c8dadbb28318cb51e1bdca
Nick Barone <xnickbarone@gmail.com>
2020-05-18 15:49:47 -0400

    Working pagination

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -5,10 +5,11 @@ import gql from 'graphql-tag';
 
 import Item from './Item';
 import Pagination from './Pagination';
+import { perPage } from '../config';
 
 const ALL_ITEMS_QUERY = gql`
-	query ALL_ITEMS_QUERY {
-		items {
+	query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+		items(first: $first, skip: $skip, orderBy: createdAt_DESC) {
 			id
 			title
 			price
@@ -35,7 +36,7 @@ const Items = ({ page }) => {
 	return (
 		<Center>
 			<Pagination page={page} />
-			<Query query={ALL_ITEMS_QUERY}>
+			<Query query={ALL_ITEMS_QUERY} fetchPolicy="network-only" variables={{ skip: page * perPage - perPage }}>
 				{({ data, error, loading }) => {
 					if (loading) return <p>Loading...</p>;
 					if (error) return <p>Error: {error.message}</p>;
