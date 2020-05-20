217a009a81acb0534b3fb6308bc2e8a157bb72e2
Nick Barone <xnickbarone@gmail.com>
2020-05-19 23:52:57 -0400

    Convert to hook

--- sick-fits/frontend/components/UpdateItem.js
+++ sick-fits/frontend/components/UpdateItem.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Query, Mutation } from 'react-apollo';
+import { Mutation } from 'react-apollo';
+import { useQuery } from '@apollo/react-hooks';
 import Router from 'next/router';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
@@ -35,82 +36,81 @@ const UpdateItem = ({ id }) => {
 	const [price, setPrice] = useState('');
 	const [description, setDescription] = useState('');
 
+	const { loading, error, data } = useQuery(SINGLE_ITEM_QUERY, {
+		variables: { id: id },
+	});
+
+	if (loading) return <p>Loading...</p>;
+	if (error) return <Error error={error} />;
+	if (!data.item) return <p>No Item Found</p>;
+
 	return (
-		<Query query={SINGLE_ITEM_QUERY} variables={{ id: id }}>
-			{({ data, loading }) => {
-				if (loading) return <p>Loading...</p>;
-				if (!data.item) return <p>No Item Found</p>;
-
-				return (
-					<Mutation
-						mutation={UPDATE_ITEM_MUTATION}
-						variables={{
-							title: data.item.title,
-							price: data.item.price,
-							description: data.item.description,
-						}}
-					>
-						{(updateItem, { loading, error }) => (
-							<Form
-								onSubmit={async e => {
-									e.preventDefault();
-									const response = await updateItem({
-										variables: {
-											id,
-											title: title ? title : data.item.title,
-											price: price ? price : data.item.price,
-											description: description ? description : data.item.description,
-										},
-									});
-								}}
-							>
-								<Error errpr={error} />
-								<fieldset disabled={loading} aria-busy={loading}>
-									<label htmlFor="title">
-										Title
-										<input
-											type="text"
-											id="title"
-											name="title"
-											placeholder="Title"
-											required
-											defaultValue={data.item.title}
-											onChange={e => setTitle(e.target.value)}
-										/>
-									</label>
-
-									<label htmlFor="price">
-										Price
-										<input
-											type="number"
-											id="price"
-											name="price"
-											placeholder="Price"
-											required
-											defaultValue={data.item.price}
-											onChange={e => setPrice(parseFloat(e.target.value))}
-										/>
-									</label>
-
-									<label htmlFor="description">
-										Description
-										<textarea
-											id="description"
-											name="description"
-											placeholder="Enter A Description"
-											required
-											defaultValue={data.item.description}
-											onChange={e => setDescription(e.target.value)}
-										/>
-									</label>
-									<button type="submit">Sav{loading ? 'ing' : 'e'} Changes</button>
-								</fieldset>
-							</Form>
-						)}
-					</Mutation>
-				);
+		<Mutation
+			mutation={UPDATE_ITEM_MUTATION}
+			variables={{
+				title: data.item.title,
+				price: data.item.price,
+				description: data.item.description,
 			}}
-		</Query>
+		>
+			{(updateItem, { loading, error }) => (
+				<Form
+					onSubmit={async e => {
+						e.preventDefault();
+						const response = await updateItem({
+							variables: {
+								id,
+								title: title ? title : data.item.title,
+								price: price ? price : data.item.price,
+								description: description ? description : data.item.description,
+							},
+						});
+					}}
+				>
+					<Error error={error} />
+					<fieldset disabled={loading} aria-busy={loading}>
+						<label htmlFor="title">
+							Title
+							<input
+								type="text"
+								id="title"
+								name="title"
+								placeholder="Title"
+								required
+								defaultValue={data.item.title}
+								onChange={e => setTitle(e.target.value)}
+							/>
+						</label>
+
+						<label htmlFor="price">
+							Price
+							<input
+								type="number"
+								id="price"
+								name="price"
+								placeholder="Price"
+								required
+								defaultValue={data.item.price}
+								onChange={e => setPrice(parseFloat(e.target.value))}
+							/>
+						</label>
+
+						<label htmlFor="description">
+							Description
+							<textarea
+								id="description"
+								name="description"
+								placeholder="Enter A Description"
+								required
+								defaultValue={data.item.description}
+								onChange={e => setDescription(e.target.value)}
+							/>
+						</label>
+						<button type="submit">Sav{loading ? 'ing' : 'e'} Changes</button>
+					</fieldset>
+				</Form>
+			)}
+		</Mutation>
 	);
 };
 
