e3b0a83a13e19a78875fc825b91c62068661e37d
Nick Barone <xnickbarone@gmail.com>
2020-05-21 00:57:45 -0400

    Convert User component to a hook per updated guide, eliminates double render

--- sick-fits/frontend/components/User.js
+++ sick-fits/frontend/components/User.js
@@ -1,6 +1,5 @@
 import { useQuery } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
-import PropTypes from 'prop-types';
 
 const CURRENT_USER_QUERY = gql`
 	query {
@@ -13,18 +12,11 @@ const CURRENT_USER_QUERY = gql`
 	}
 `;
 
-const User = ({ children }) => {
-	const payload = useQuery(CURRENT_USER_QUERY);
-
-	// TODO issue with this, chapter 26/27
-	if (!payload.data) return null;
-
-	return children(payload);
+const useUser = () => {
+	const { data, loading, error } = useQuery(CURRENT_USER_QUERY);
+	if (data) {
+		return data.me;
+	}
 };
 
-User.propTypes = {
-	children: PropTypes.func.isRequired,
-};
-
-export default User;
-export { CURRENT_USER_QUERY };
+export { CURRENT_USER_QUERY, useUser };
